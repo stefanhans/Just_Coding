1fe11d35bd859d680b3615535920d28a6ae028b2
Stefan Hans <uni@stefan-hans.de>
2017-10-20 21:17:36 +0200

    Finished Session 2

--- bcgo/README.md
+++ bcgo/README.md
@@ -3,8 +3,26 @@
 [Designed after a blog by Ivan Kuznetsov.](https://jeiwan.cc/)
 
 
+## Branch: Session_1
+
+Until end of part 2: Proof-of-Work
+
 Experiences:
 
 - No `go run main.go`, use `go build;bcgo` instead
+
+## Branch: Session_2
+
+Until end of part 3: Persistence and CLI
+
+Experiences:
+
+- Introducing GitHub branches
+- Don't forget `-data` and delete `blockchain.db` if needed
+- Changed order of blockchain output lines, i.e. `Prev. Hash` from first to last line
+
+
+
+
 
 
--- bcgo/block.go
+++ bcgo/block.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	_ "bytes"
-	_ "crypto/sha256"
-	_ "strconv"
-	"time"
 	"bytes"
 	"encoding/gob"
-	"fmt"
+	"log"
+	"time"
 )
 
+// Block keeps block headers
 type Block struct {
 	Timestamp     int64
 	Data          []byte
@@ -18,36 +16,45 @@ type Block struct {
 	Nonce         int
 }
 
-/* Commented out in lesson 2
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-	b.Hash = hash[:]
-
-}
-*/
-
+// Serialize serializes the block
 func (b *Block) Serialize() []byte {
 	var result bytes.Buffer
 	encoder := gob.NewEncoder(&result)
 
-	if err := encoder.Encode(b); err != nil {
-		fmt.Printf("%s \n", err)
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
 	}
-
-
 
 	return result.Bytes()
 }
 
+// NewBlock creates and returns Block
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
 	pow := NewProofOfWork(block)
 	nonce, hash := pow.Run()
 
-	block.Hash = hash
+	block.Hash = hash[:]
 	block.Nonce = nonce
 
 	return block
+}
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+// DeserializeBlock deserializes a block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
